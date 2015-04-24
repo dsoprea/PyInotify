f0e79f34be0cd484cd5b5dd06b137119078c07df
Dustin Oprea <myselfasunder@gmail.com>
2015-04-24 03:43:41 -0400

    Adjustments. Added recursiveness.
    - Added InotifyTree (for recursive watching).
    - Added 'superficial' parameter to remove_watch() to just remove tracking.
    - We now yield the watched path name.
    - We now strip the trailing NULs from the filename.

--- inotify/adapters.py
+++ inotify/adapters.py
@@ -33,6 +33,7 @@ class Inotify(object):
     def __init__(self, paths=[], block_duration_s=_DEFAULT_EPOLL_BLOCK_DURATION_S):
         self.__block_duration_s = block_duration_s
         self.__watches = {}
+        self.__watches_r = {}
         self.__buffer = ''
 
         self.__inotify_fd = inotify.calls.inotify_init()
@@ -50,17 +51,30 @@ class Inotify(object):
         os.close(self.__inotify_fd)
 
     def add_watch(self, path, mask=inotify.constants.IN_ALL_EVENTS):
+        _LOGGER.debug("Adding watch: [%s]", path)
+
         wd = inotify.calls.inotify_add_watch(self.__inotify_fd, path, mask)
         _LOGGER.debug("Added watch (%d): [%s]", wd, path)
 
         self.__watches[path] = wd
+        self.__watches_r[wd] = path
 
-    def remove_watch(self, path):
+    def remove_watch(self, path, superficial=False):
+        """Remove our tracking information and call inotify to stop watching 
+        the given path. When a directory is removed, we'll just have to remove 
+        our tracking since inotify already cleans-up the watch.
+        """
+
         wd = self.__watches[path]
-        _LOGGER.debug("Removing watch for Inotify handle (%d) and watch-"
-                      "handle (%d): [%s]", self.__inotify_fd, wd, path)
 
-        inotify.calls.inotify_rm_watch(self.__inotify_fd, wd)
+        del self.__watches[path]
+        del self.__watches_r[wd]
+
+        if superficial is False:
+            _LOGGER.debug("Removing watch for watch-handle (%d): [%s]", 
+                          wd, path)
+
+            inotify.calls.inotify_rm_watch(self.__inotify_fd, wd)
 
     def __get_event_names(self, event_type):
         names = []
@@ -111,9 +125,14 @@ class Inotify(object):
                 return
 
             filename = self.__buffer[_STRUCT_HEADER_LENGTH:event_length]
+
+            # Our filename is 16-byte aligned and right-padded with NULs.
+            filename = filename.rstrip('\0')
+
             self.__buffer = self.__buffer[event_length:]
 
-            yield (header, type_names, filename)
+            path = self.__watches_r[header.wd]
+            yield (header, type_names, path, filename)
 
             buffer_length = len(self.__buffer)
             if buffer_length < _STRUCT_HEADER_LENGTH:
@@ -123,8 +142,74 @@ class Inotify(object):
         while True:
             events = self.__epoll.poll(self.__block_duration_s)
             for fd, event_type in events:
-                for (header, type_names, filename) \
+                # (fd) looks to always match the inotify FD.
+
+                for (header, type_names, path, filename) \
                         in self.__handle_inotify_event(fd, event_type):
-                    yield (header, type_names, filename)
+                    yield (header, type_names, path, filename)
 
             yield None
+
+
+class InotifyTree(object):
+    def __init__(self, path, mask=inotify.constants.IN_ALL_EVENTS, 
+                 block_duration_s=_DEFAULT_EPOLL_BLOCK_DURATION_S):
+
+        self.__root_path = path
+
+        # No matter what we actually received as the mask, make sure we have 
+        # the minimum that we require to curate our list of watches.
+        self.__mask = mask | \
+                        inotify.constants.IN_ISDIR | \
+                        inotify.constants.IN_CREATE | \
+                        inotify.constants.IN_DELETE
+
+        self.__i = Inotify(block_duration_s=block_duration_s)
+
+        self.__load_tree(path)
+
+    def __load_tree(self, path):
+        _LOGGER.debug("Adding initial watches on tree: [%s]", path)
+
+        q = [path]
+        while q:
+            current_path = q[0]
+            del q[0]
+
+            self.__i.add_watch(current_path, self.__mask)
+
+            for filename in os.listdir(current_path):
+                entry_filepath = os.path.join(current_path, filename)
+                if os.path.isdir(entry_filepath) is False:
+                    continue
+
+                q.append(entry_filepath)
+
+    def event_gen(self):
+        """This is a secondary generator that wraps the principal one, and 
+        adds/removes watches as directories are added/removed.
+        """
+
+        for event in self.__i.event_gen():
+            if event is not None:
+                (header, type_names, path, filename) = event
+
+                if header.mask & inotify.constants.IN_ISDIR:
+                    full_path = os.path.join(path, filename)
+
+                    if header.mask & inotify.constants.IN_CREATE:
+                        _LOGGER.debug("A directory has been created. We're "
+                                      "adding a watch on it (because we're "
+                                      "being recursive): [%s]", full_path)
+
+                        self.__i.add_watch(full_path, self.__mask)
+                    elif header.mask & inotify.constants.IN_DELETE:
+                        _LOGGER.debug("A directory has been removed. We're "
+                                      "being recursive, but it would have "
+                                      "automatically been deregistered: [%s]", 
+                                      full_path)
+
+                        # The watch would've already been cleaned-up internally.
+                        self.__i.remove_watch(full_path, superficial=True)
+
+            yield event
